ad4f100afb13268a41fe7ff47c9cf91e77a6b7b1
Igor Heleno <igor.campos@rochapecas.com.br>
2025-06-04 12:41:31 -0300

    feat: implement EnvModule and EnvService for environment configuration management

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from './auth/auth.module'
-import { envSchema } from './env'
+
 import { HttpModule } from './http/http.module'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
@@ -11,8 +13,10 @@ import { HttpModule } from './http/http.module'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+
     AuthModule,
     HttpModule,
+    EnvModule,
   ],
 })
 export class AppModule {}
