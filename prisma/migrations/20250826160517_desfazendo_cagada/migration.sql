2c307bd6f0c40da248269ec8f0b1265f83fb9483
heleno2307 <igor.campos@rochapecas.com.br>
2025-08-26 14:27:45 -0300

    finalizando controller de comentário na pergunta

---
+++ prisma/migrations/20250826160517_desfazendo_cagada/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."comments" ALTER COLUMN "updated_at" DROP NOT NULL;
---
+++ src/infra/http/controllers/comment-on-question.controller.ts
@@ -0,0 +1,44 @@
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Param,
+    Post,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { TokenSchema } from '@/infra/auth/jwt.strategy'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+
+const commentOnQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+type CommentOnQuestionBodySchema = z.infer<typeof commentOnQuestionBodySchema>
+
+@Controller('/questions/:questionId/comments')
+export class CommentOnQuestionController {
+  constructor(private commentOnQuestion: CommentOnQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(new ZodValidationPipe(commentOnQuestionBodySchema))
+    body: CommentOnQuestionBodySchema,
+    @Param('questionId') questionId: string,
+    @CurrentUser() user: TokenSchema,
+  ) {
+    const { content } = body
+    const userId = user.sub
+
+    const result = await this.commentOnQuestion.execute({
+      content,
+      questionId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
